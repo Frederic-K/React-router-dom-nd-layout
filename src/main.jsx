c18ff8f1ecf19b670e911388a088b6b0358a8b01
Frederic-K <frederic.kreuder.pro@gmail.com>
2024-04-14 18:57:11 +0200

    squizze App to test routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,57 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import User from './pages/User'
+import PageError from './pages/PageError'
+import Root from './components/Root'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <PageError />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+        children: [
+          {
+            path: ':id',
+            element: <User />,
+          },
+        ],
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+])
+
+// function Root() {
+//   return (
+//     <>
+//       <Header />
+//       <Container sx={{ my: '10px' }}>
+//         <Outlet />
+//       </Container>
+//       <Footer />
+//     </>
+//   )
+// }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
